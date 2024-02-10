bf380e5e441fd66e741ef9011d0917843ca6f1bf
Ravishankar Shetgar <shetgarravi.1123@gmail.com>
2024-02-10 14:28:04 +0530

    Add constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,19 @@
+enum COLORS {
+  blue300 = '#00202e',
+  blue200 = '#003f5c',
+  blue100 = '#2c4875',
+  blue = '#48628c',
+  purple200 = '#8a508f',
+  purple100 = '#bc5090',
+  orange = '#ff6361',
+  yellow300 = '#ff8531',
+  yellow200 = '#ffa600',
+  yellow100 = '#ffd380',
+  white = '#fff',
+  grey100 = '#ebebeb',
+  grey200 = '#adb0b2',
+  grey300 = '#898e8c',
+  black = '#000',
+}
+
+export default COLORS;
---
+++ src/constants/common.ts
@@ -0,0 +1,10 @@
+export enum FONTS {
+  LatoBold = 'Lato-Bold',
+  LatoExtraBold = 'Lato-ExtraBold',
+  LatoExtraLight = 'Lato-ExtraLight',
+  LatoLight = 'Lato-Light',
+  LatoMedium = 'Lato-Medium',
+  LatoRegular = 'Lato-Regular',
+  LatoSemiBold = 'Lato-SemiBold',
+  LatoThin = 'Lato-Thin',
+}
